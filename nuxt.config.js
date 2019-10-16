24ac1c44e14207919616b2aff9974f2b1c20b3c8
Colin Brayton <gringolalia@gmail.com>
2019-10-16 19:51:01 -0300

    initiating countdown engines on

---
+++ nuxt.config.js
@@ -0,0 +1,69 @@
+const pkg = require('./package')
+
+module.exports = {
+  mode: 'spa',
+
+  /*
+  ** Headers of the page
+  */
+  head: {
+    script: [
+      // { src: 'https://cdnjs.cloudflare.com/ajax/libs/uikit/3.2.1/js/uikit.min.js' },
+      // { src: 'https://cdnjs.cloudflare.com/ajax/libs/uikit/3.2.1/js/uikit.min.js' },
+      // { src: 'https://cdnjs.cloudflare.com/ajax/libs/uikit/3.2.1/js/uikit-icons.min.js' },
+      { src: 'node_modules/uikit/dist/js/uikit.min.js' },
+      { src: 'node_modules/uikit/dist/js/uikit-core.min.js' },
+      { src: 'https://cdnjs.cloudflare.com/ajax/libs/uikit/3.2.1/js/uikit-icons.min.js' },
+    ],
+    title: pkg.name,
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: pkg.description }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', type: 'text/csss', href: 'https://cdnjs.cloudflare.com/ajax/libs/uikit/3.2.1/css/uikit.min.css' },
+      { rel: 'stylesheet', type: 'text/csss', href: 'https://fonts.googleapis.com/css?family=News+Cycle|Material+Icons' },
+      { rel: 'stylesheet', type: 'text/csss', href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css' },
+    ]
+  },
+
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+
+  /*
+  ** Global CSS
+  */
+  css: [
+      '@/assets/scss/main.scss'
+  ],
+
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+    { src: '~/plugins/uikit.js', ssr: false }
+  ],
+
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    // '@nuxtjs/vuetify',
+  ],
+
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend(config, ctx) {
+      
+    }
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,57 @@
+<template lang="pug">
+
+section.container
+  div
+    img(src="../assets/images/hammer-sickle.svg" width="150px")
+    h1.uk-heading-xlarge Youth In Kuwait
+      
+    h1.uk-heading-medium You and I in the Kitchen
+    button.uk-button.uk-button-default Imprints
+    button.uk-button.uk-button-secondary Offprints
+    button.uk-button.uk-button-dark Footprints
+
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
+
+<style>
+
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'News Cycle', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-family: "News Cycle", sans-serif;
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
